40a519fa6496a6a55c1fbb0f4ad33db018654327
Daniel Kroening <dkr@amazon.com>
2022-05-26 10:09:37 +0100

    pointer comparison now unsigned

--- regression/cprover/pointers/pointer_logic3.c
+++ regression/cprover/pointers/pointer_logic3.c
@@ -4,12 +4,15 @@ int main()
   __CPROVER_size_t size, offset;
 
   __CPROVER_assume(__CPROVER_r_ok(base, size));
-  __CPROVER_assume(offset < size);
 
-  p = base + offset;
+  if(offset < size)
+  {
+    p = base + offset;
 
-  __CPROVER_assert(base <= p, "property 1"); // should pass
-  __CPROVER_assert(p < base + size , "property 2"); // should pass
+    __CPROVER_assert(__CPROVER_same_object(base, p), "property 1"); // should pass
+    __CPROVER_assert(base <= p, "property 2"); // should pass
+    __CPROVER_assert(p < base + size , "property 3"); // should pass
+  }
 
   return 0;
 }
