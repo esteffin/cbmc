4ab09d64f9f1e1f6e167f7790f9bcb52120ab655
Enrico Steffinlongo <enrico.steffinlongo@diffblue.com>
2023-04-24 16:41:57 +0100

    Changed main structure to old parse_option-doIt

--- src/run-symex-only/main.cpp
+++ src/run-symex-only/main.cpp
@@ -1,42 +1,11 @@
 // Author: Enrico Steffinlongo for Diffblue Ltd.
 
-#include <util/exception_utils.h>
-
-#include <libcprover-cpp/api.h>
-#include <libcprover-cpp/api_options.h>
-
-#include "corebmc_parse_options.h"
+#include "core_cbmc_only_parse_options.h"
 
 #include <iostream>
 #include <vector>
 
-void print_messages_to_stdout(
-  const api_messaget &message,
-  api_call_back_contextt)
+int main(int argc, const char **argv)
 {
-  std::cout << api_message_get_string(message) << std::endl;
-}
-
-int main(int argc, char *argv[])
-{
-  // Parsing of arguments works on vector, so we need to do some type-jitsu
-  std::vector<std::string> arguments(argv + 1, argv + argc);
-  auto api_options = parse_command_line_options(arguments);
-
-  // With options set, we can now initiate the API and perform analysis.
-  api_sessiont api(api_options);
-  std::cout << "CoreBMC version ";
-  std::cout << *api.get_api_version() << std::endl;
-
-  // The API works with a callback for querying state - we for now just print
-  // collected messages from the message buffer to stdout.
-  api.set_message_callback(print_messages_to_stdout, nullptr);
-
-  // Finally, we read the goto-binary the user supplied...
-  api.read_goto_binary(arguments[0]);
-
-  // ... and run analysis on it.
-  auto result = api.verify_model();
-
-  return 0;
+  return core_cbmc_only_parse_optionst{argc, argv}.main();
 }
