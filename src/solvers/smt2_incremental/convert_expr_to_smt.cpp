7b09abc2234d2d5983a1ac4c3c4552161f3040b2
Enrico Steffinlongo <enrico.steffinlongo@diffblue.com>
2023-08-25 15:00:21 +0100

    Add bool type case to extension_for_type on SMT2 solver

--- src/solvers/smt2_incremental/convert_expr_to_smt.cpp
+++ src/solvers/smt2_incremental/convert_expr_to_smt.cpp
@@ -165,6 +165,8 @@ extension_for_type(const typet &type)
     return smt_bit_vector_theoryt::zero_extend;
   if(can_cast_type<bv_typet>(type))
     return smt_bit_vector_theoryt::zero_extend;
+  if(can_cast_type<c_bool_typet>(type))
+    return smt_bit_vector_theoryt::zero_extend;
   UNREACHABLE;
 }
 
